cb0d59082a4b1f04f23463d63319241b64914cd2
Etapi017 <etapi017@ucr.edu>
2024-05-26 14:49:43 -0700

    Include image URL

--- models/item.model.js
+++ models/item.model.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const itemSchema = new Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
+    imageUrl: { type: String, required: true },
 }, {
     timestamps: true,
 });
