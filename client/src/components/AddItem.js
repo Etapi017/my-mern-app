52bd3c232cb3a96b64c2c53154fc5890713e4b6d
Etapi017 <etapi017@ucr.edu>
2024-05-26 14:54:00 -0700

    Handle file uploads

--- client/src/components/AddItem.js
+++ client/src/components/AddItem.js
@@ -4,20 +4,22 @@ import axios from 'axios';
 const AddItem = () => {
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
+    const [image, setImage] = useState(null);
 
     const onSubmit = (e) => {
         e.preventDefault();
 
-        const newItem = {
-            name,
-            description,
-        };
+        const formData = new FormData();
+        formData.append('name', name);
+        formData.append('description', description);
+        formData.append('image', image);
 
-        axios.post('http://localhost:5000/items/add', newItem)
+        axios.post('http://localhost:5000/items/add', formData)
             .then(res => console.log(res.data));
 
         setName('');
         setDescription('');
+        setImage(null);
     };
 
     return (
@@ -43,11 +45,18 @@ const AddItem = () => {
                     />
                 </div>
                 <div>
+                    <label>Image: </label>
+                    <input
+                        type="file"
+                        onChange={(e) => setImage(e.target.files[0])}
+                    />
+                </div>
+                <div>
                     <input type="submit" value="Add Item" />
                 </div>
             </form>
         </div>
     );
-};
+    };
 
-export default AddItem;
+    export default AddItem;
